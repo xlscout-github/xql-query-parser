b7abed94d1467af0f4702019a01541a3652e294b
Simran Dhawan <simran.dhawan@xlpat.com>
2021-06-23 09:52:43 +0530

    XQL v2

--- sample.js
+++ sample.js
@@ -8,6 +8,12 @@ const { parse } = require("./parser");
 
 // console.dir(pickedKeyParsedResult, { depth: null });
 
-const result = parse(`((( tac: ((sunitinib OR sutent ) AND (topical OR surface) AND (composition OR formulat*)) AND text: ((treating* OR curi*) AND ((ocular OR eye OR optic OR opthalmic OR visual) near5 (disorder))) AND tac: (("amd" OR "dme" OR "rvo"))) AND ifi_patstat: (pending OR active OR in-force )) AND pnctry: (AU OR CA OR DE OR EP OR ES OR FR OR GB OR JP OR US))`);
+const result = parse(
+  `((( tac: ((sunitinib OR sutent ) AND (topical OR surface) AND (composition OR formulat*)) AND text: ((treating* OR curi*) AND ((ocular OR eye OR optic OR opthalmic OR visual) near5 (disorder))) AND tac: (("amd" OR "dme" OR "rvo"))) AND ifi_patstat: (pending OR active OR in-force )) AND pnctry: (AU OR CA OR DE OR EP OR ES OR FR OR GB OR JP OR US))`
+);
 
 console.dir(result, { depth: null });
+
+const finalEQ = EQLgenerator(JSON.stringify(result));
+
+console.dir(finalEQ, { depth: null });
