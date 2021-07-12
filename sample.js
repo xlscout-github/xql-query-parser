5063ab72065d1e84cea4303c8422c87916cde459
Utkarsh Chowdhary <utkarsh.chowdhary@xlpat.com>
2021-07-12 19:08:15 +0530

    added comments t sample.js file

--- sample.js
+++ sample.js
@@ -1,9 +1,11 @@
 const { parse } = require("./parser");
 
+// core parse
 const result = parse(`((desc:(DETECT* near5 (CONNECT* near6 SOURCE*)))) OR pn:US7420295B2`);
 
 console.dir(result, { depth: null });
 
+// condensed parse
 const result_c = parse(`((desc:(DETECT* near5 (CONNECT* near6 SOURCE*)))) OR pn:US7420295B2`, true);
 
 console.dir(result_c, { depth: null });
