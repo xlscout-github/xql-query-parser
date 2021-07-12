ccc03429f7ba2c4538ca3cc0a41186d71423e049
Utkarsh Chowdhary <utkarsh.chowdhary@xlpat.com>
2021-07-12 19:02:06 +0530

    added argument to parse to trigger transform_condense fun

--- sample.js
+++ sample.js
@@ -1,5 +1,9 @@
 const { parse } = require("./parser");
 
-const result = parse(`(_v_:0.5.0)`);
+const result = parse(`((desc:(DETECT* near5 (CONNECT* near6 SOURCE*)))) OR pn:US7420295B2`);
 
 console.dir(result, { depth: null });
+
+const result_c = parse(`((desc:(DETECT* near5 (CONNECT* near6 SOURCE*)))) OR pn:US7420295B2`, true);
+
+console.dir(result_c, { depth: null });
