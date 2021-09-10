1e7de46c4f302ba9b0b9663728215708c7c221b1
Utkarsh Chowdhary <utkarsh.chowdhary@xlpat.com>
2021-09-10 21:18:11 +0530

    used negation range regex instead for specifying supported characters

--- sample.js
+++ sample.js
@@ -1,13 +1,5 @@
 const { parse } = require("./parser");
-// const { EQLgenerator } = require("./EQLgenerator");
-
-let result = parse(`pdyear:(([2020 TO *])) AND ipc.grp:(G06F30)`);
-
-console.dir(result, { depth: null });
-
-// result = EQLgenerator(result);
-
-// condensed parse
-// const result_c = parse(`((desc:(DETECT* near5 (CONNECT* near6 SOURCE*)))) OR pn:US7420295B2`, true);
-
-// console.dir(result_c, { depth: null });
+// pdyear:(([2020 TO *])) AND ipc.grp:(Telefônicas)
+console.dir(parse(`pdyear:(([2020 TO *])) AND ipc.grp:(Telefônicas)`), {
+  depth: null,
+});
