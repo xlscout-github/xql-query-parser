c84b2c463ceb46e859f4803cdc279b30d1149bb8
Utkarsh Chowdhary <utkarsh.chowdhary@xlpat.com>
2021-09-11 00:09:37 +0530

    removed en-dash from solo value

--- sample.js
+++ sample.js
@@ -1,5 +1,5 @@
 const { parse } = require("./parser");
-// pdyear:(([2020 TO *])) AND ipc.grp:(Telefônicas)
+
 console.dir(parse(`pdyear:(([2020 TO *])) AND ipc.grp:(Telefônicas)`), {
   depth: null,
 });
