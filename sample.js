b13b70549e4e9defe6dafda478c0803df6663710
Utkarsh Chowdhary <utkarsh.chowdhary@xlpat.com>
2021-09-11 08:55:51 +0530

    added some special characters to range regex

--- sample.js
+++ sample.js
@@ -1,5 +1,10 @@
 const { parse } = require("./parser");
 
-console.dir(parse(`pdyear:(([2020 TO *])) AND ipc.grp:(Telefônicas)`), {
-  depth: null,
-});
+console.dir(
+  parse(
+    `ipc:",.<>/?;\\|~Te!@l#$e%fôn^&i*-_=+cas" AND ipc.grp:(,.<>/?;\\|~Te!@l#$e%fôn^&i*-_=+cas)`
+  ),
+  {
+    depth: null,
+  }
+);
